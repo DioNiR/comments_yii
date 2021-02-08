92d0577546fdea91b67f7b85b89c66ef36838825
DioNiR <dionir90@gmail.com>
2021-02-08 10:23:21 +0300

    Migrate

---
+++ migrations/m210208_063228_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%comments}}`.
+ */
+class m210208_063228_create_comments_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%comments}}', [
+            'id' => $this->primaryKey(),
+            'parentId' => $this->integer()->defaultValue(0),
+            'date' => $this->dateTime()->defaultExpression('CURRENT_TIMESTAMP'),
+            'comment_text' => $this->text()->notNull(),
+            'authorId' => $this->char(100),
+            'author_name' => $this->char(10),
+            'hide' => $this->tinyInteger(1),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%comments}}');
+    }
+}
